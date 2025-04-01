430e49f1fa870be8e3995c914189e61b86b5c42f
Francis Zhang <55003147+Tegrisco-ZSQ@users.noreply.github.com>
2025-03-31 22:38:32 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
-# importing important utitilites and libraries
-import os
+import pandas as pd
+import os 
+from dotenv import load_dotenv
 from promptflow.tracing import start_trace
-import dotenv
-from dotenv import load_dotenv
-from dotenv import load_dotenv
 from promptflow.core import Prompty, AzureOpenAIModelConfiguration
-from promptflow.client import load_flow
+from promptflow.evals.evaluators import RelevanceEvaluator, GroundednessEvaluator, ViolenceEvaluator
 
-#starting the trace
-load_dotenv()
+#opening the csv file in reading mode using pandas dataframe
+df=pd.read_csv('evaluation_ai_studio.csv')
+num_columns = df.shape[1] #counting the number of columns in the csv file
+num_rows = df.shape[0] #counting the number of rows in the csv file
+
+#intializing the count variable to 0
+count=0
+
 #creating the AzureOpenAIModelConfiguration object
+load_dotenv()
 model_config = AzureOpenAIModelConfiguration(
     azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
     api_key=os.getenv("OPENAI_KEY"),
@@ -17,11 +22,52 @@ model_config = AzureOpenAIModelConfiguration(
     api_version="2024-05-01-preview"
 )
 
-#loading the custom prompt evaluator
-courtesy_eval = load_flow(source="evaluator.prompty", model = {"configuration": model_config})
-#evaluating the courtesy score
-courtesy_score = courtesy_eval(question="hello how are you?", answer="I am fine. You need to ask me that again")
+#creating the RelevanceEvaluator and GroundednessEvaluator objects from the promptflow.evals.evaluators module in the promptflow-evals SDK for python
+relevance_eval = RelevanceEvaluator(model_config)
+groundedness_eval = GroundednessEvaluator(model_config)
 
-#displaying the courtesy score
-print(courtesy_score)
+#creating the lists to store the relevance and groundedness scores
+relevance_score_list = []
+groundedness_score_list = []
+
+#looping through the rows of the csv file
+while(count<num_rows):
+    #evaluating the relevance and groundedness scores for each question-answer pair in the csv file
+    relevance_score= relevance_eval(
+        answer=df.iloc[count][2],
+        context=df.iloc[count][1],
+        question=df.iloc[count][0]
+    )
+    
+    groundedness_score = groundedness_eval(
+        answer = df.iloc[count][2],
+        context = df.iloc[count][1],
+    )   
+    
+    
+    #printing the question, answer, relevance score and groundedness score for each question-answer pair
+    print("question : " + df.iloc[count][0])
+    print("answer : " + df.iloc[count][2])
+    
+    #converting the relevance and groundedness scores to string and printing them from the dict values
+    gpt_relevance_score=str(relevance_score['gpt_relevance'])
+    gpt_groundedness_score = str(groundedness_score['gpt_groundedness'])
+    
+    #printing the relevance and groundedness scores
+    print("relevance score : " + gpt_relevance_score)
+    print("groundedness score : " + gpt_groundedness_score)
+    
+    #appending the relevance and groundedness scores to the relevance_score_list and groundedness_score_list
+    relevance_score_list.append(gpt_relevance_score)
+    groundedness_score_list.append(gpt_groundedness_score) 
+   
+   # incrementing the count variable
+    count+=1
+    
+#adding the relevance and groundedness scores to the csv file    
+df['GPT Relevance Score'] = relevance_score_list
+df['GPT Groundedness Score'] = groundedness_score_list
+
+#saving the csv file with the relevance and groundedness scores
+df.to_csv('evaluation_ai_studio.csv', index=False)
 
